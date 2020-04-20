b218c820cf9515205d0f33f4741e6342dba8f221
Arpo-Two <63409416+Arpo-Two@users.noreply.github.com>
2020-04-20 12:13:27 -0300

    Added wall-breaking
    Smoothed the code in some parts and added a wall-breaking feature, in which  the finder can break n walls in its pathfinding

--- Pathfinding.py
+++ Pathfinding.py
@@ -17,6 +17,7 @@ block_color = (75, 0, 130)
 finder_color = (220, 20, 60)
 end_color = (255, 255, 0)
 path_color = (0, 255, 127)
+over_block_color = (60, 179, 113)
 menu_color = (0, 0, 205)
 menu_color2 = (255, 165, 0)
 
@@ -25,6 +26,7 @@ key = [[0, 0], [grid_size - 1, grid_size - 1]]
 lock_find = False
 lock_end = False
 show_path = False
+wall_breaks = 0
 
 
 def distance(object1, object2):
@@ -73,65 +75,74 @@ def draw_menu():
     pygame.draw.circle(win, black, (talt + 75, 3 * talt // 8 - 25), 25, 1)
     win.blit(big.render('-', False, black), (talt + 75 - 10, 3 * talt // 8 - 55))
 
+    pygame.draw.circle(win, menu_color, (talt + 75, 3 * talt // 4 + 25), 50)
+    pygame.draw.circle(win, black, (talt + 75, 3 * talt // 4 + 25), 50, 1)
+    win.blit(big.render(str(wall_breaks), False, black), (talt + 75 - 10, 3 * talt // 4 - 8))
+
+    pygame.draw.circle(win, menu_color2, (talt + 75, 7 * talt // 8 + 25), 25)
+    pygame.draw.circle(win, black, (talt + 75, 7 * talt // 8 + 25), 25, 1)
+    win.blit(big.render('+', False, black), (talt + 75 - 15, 7 * talt // 8 - 5))
+
+    pygame.draw.circle(win, menu_color2, (talt + 75, 5 * talt // 8 + 25), 25)
+    pygame.draw.circle(win, black, (talt + 75, 5 * talt // 8 + 25), 25, 1)
+    win.blit(big.render('-', False, black), (talt + 75 - 10, 5 * talt // 8 - 5))
+
 
 def pathfind(start, end):
-    queue = [[start[0], start[1], 0]]
+    queue = [[[start[0], start[1], 0, wall_breaks]]]
+    tries = 0
     while True:
+        if tries > grid_size ** 2:
+            return []
+        tries += 1
         for q in queue:
-            if q[0] == end[0] and q[1] == end[1]:
-                return queue
+            if q[-1][0] == end[0] and q[-1][1] == end[1]:
+                return q
             to_remove = []
-            adj = [[q[0] + 1, q[1], q[2] + 1],
-                   [q[0] - 1, q[1], q[2] + 1],
-                   [q[0], q[1] - 1, q[2] + 1],
-                   [q[0], q[1] + 1, q[2] + 1]]
+            adj = [[q[-1][0] + 1, q[-1][1], q[-1][2] + 1, q[-1][3]],
+                   [q[-1][0] - 1, q[-1][1], q[-1][2] + 1, q[-1][3]],
+                   [q[-1][0], q[-1][1] - 1, q[-1][2] + 1, q[-1][3]],
+                   [q[-1][0], q[-1][1] + 1, q[-1][2] + 1, q[-1][3]]]
             for a in adj:
                 if a[0] < 0 or a[1] < 0 or a[0] >= grid_size or a[1] >= grid_size:
                     to_remove.append(a)
                 elif [a[0], a[1]] in blocks:
-                    to_remove.append(a)
-                for qi in queue:
-                    if qi[0] == a[0] and qi[1] == a[1] and qi[2] <= a[2]:
+                    if a[3] > 0:
+                        a[3] -= 1
+                    else:
                         to_remove.append(a)
+                for qi in queue:
+                    if qi[-1][0] == a[0] and qi[-1][1] == a[1] and qi[-1][2] <= a[2]:
+                        if qi[-1][3] >= a[3]:
+                            to_remove.append(a)
+                        else:
+                            queue = [x for x in queue if not x[-1] == qi[-1]]
             for t in to_remove:
                 if t in adj:
                     adj.remove(t)
             for a in adj:
-                queue.append(a)
-
-
-def filter_path(path):
-    filtered = []
-    for p in path:
-        if p[0] == key[1][0] and p[1] == key[1][1]:
-            filtered = [p]
-    while len(filtered) < filtered[0][2]:
-        candidates = []
-        for p in path:
-            if (p[0] == filtered[-1][0] and (p[1] == filtered[-1][1] - 1 or p[1] == filtered[-1][1] + 1)) or\
-             (p[1] == filtered[-1][1] and (p[0] == filtered[-1][0] - 1 or p[0] == filtered[-1][0] + 1)):
-                candidates.append(p)
-        choosen = candidates[0]
-        for candidate in candidates:
-            if candidate[2] < choosen[2] and [candidate[0][1]] not in blocks:
-                choosen = candidate
-        filtered.append(choosen)
-    return filtered
+                inter = q.copy()
+                inter.append(a)
+                queue.append(inter)
 
 
 def draw_path(filtered_path):
     for tile in filtered_path:
-        pygame.draw.rect(win, path_color, (tile[0] * talt / grid_size, tile[1] * talt / grid_size,
+        cor = path_color
+        for block in blocks:
+            if block[0] == tile[0] and block[1] == tile[1]:
+                cor = over_block_color
+        pygame.draw.rect(win, cor, (tile[0] * talt / grid_size, tile[1] * talt / grid_size,
                                            (talt / grid_size), (talt / grid_size)))
 
 
 run = True
 while run:
     win.fill(bg)
-    if show_path:
-        draw_path(filter_path(pathfind(key[0], key[1])))
-    draw_keys()
     draw_blocks()
+    if show_path:
+        draw_path(pathfind(key[0], key[1]))
+    draw_keys()
     draw_menu()
     draw_grid()
     for event in pygame.event.get():
@@ -157,9 +168,14 @@ while run:
                             block[0] = grid_size - 1
                         if block[1] == grid_size:
                             block[1] = grid_size - 1
-                        if blocks.count(block) > 1 or (block[0] == key[0][0] and block[1] == key[0][1]) or\
+                        if blocks.count(block) > 1 or (block[0] == key[0][0] and block[1] == key[0][1]) or \
                                 (block[0] == key[1][0] and block[1] == key[1][1]):
                             blocks = [x for x in blocks if not x == block]
+            elif distance(pygame.mouse.get_pos(), (talt + 75, 7 * talt // 8 + 25)) < 25 and not show_path:
+                wall_breaks += 1
+            elif distance(pygame.mouse.get_pos(), (talt + 75, 5 * talt // 8 + 25)) < 25 and not show_path:
+                if wall_breaks > 0:
+                    wall_breaks -= 1
 
             if pygame.mouse.get_pos()[0] < talt:
                 if pygame.mouse.get_pos()[0] // (talt / grid_size) == key[0][0] and\
